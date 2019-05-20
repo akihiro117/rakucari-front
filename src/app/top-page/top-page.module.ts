ce08cb8105b4b434b872cb88979616de480a9368
Akihiro Yamada <u.parr917@gmail.com>
2019-05-20 22:12:10 +0900

    トップページのmoduleとcomponentを作成。

---
+++ src/app/top-page/top-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopPageComponent } from './top-page/top-page.component';
+
+@NgModule({
+  declarations: [TopPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TopPageModule { }
