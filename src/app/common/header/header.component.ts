12756c892e1653f662e46b41fefd5e2fcc722909
Akihiro Yamada <u.parr917@gmail.com>
2019-05-23 22:58:43 +0900

    商品検索用メソッドと商品カテゴリー取得用メソッドを追加。

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -7,14 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public searchWords: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.fetchCategories();
   }
 
   // TODO: あとで実装する。
-  // 
+  /**
+   * APIから商品カテゴリーのリストを取得する。
+   */
   public fetchCategories(): any {
+
+  }
+
+  // TODO: あとで実装する。
+  /**
+   * APIから検索条件に類似する商品を取得する。
+   */
+  public searchItems(): any {
 
   }
 
