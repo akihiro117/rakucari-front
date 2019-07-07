d9310ad4ba8b906eb97dc302ca02799ea0686b94
Akihiro Yamada <u.parr917@gmail.com>
2019-07-07 17:26:07 +0900

    余分なimportを削除。

--- src/app/common/header/category.service.ts
+++ src/app/common/header/category.service.ts
@@ -7,7 +7,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Category } from './category';
 
 @Injectable({
   providedIn: 'root'
