b7ede38fb70b4728095b2897aa584bd3df3af01a
Akihiro Yamada <u.parr917@gmail.com>
2019-08-15 10:58:42 +0900

    app-routingに商品詳細ページへのパスを追加。

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'top', loadChildren: './top-page/top-page.module#TopPageModule'},
-  {path: '**', redirectTo: 'top'}
+  { path: 'top', loadChildren: './top-page/top-page.module#TopPageModule' },
+  { path: 'goods-detail', loadChildren: './goods-detail/goods-detail.module#GoodsDetailModule' },
+  { path: '**', redirectTo: 'top' }
 ];
 
 @NgModule({
