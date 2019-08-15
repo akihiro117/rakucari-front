ee352655221eec3d87116cac4016e03b15a0d579
Akihiro Yamada <u.parr917@gmail.com>
2019-08-15 10:38:55 +0900

    商品詳細ページのcomponentを追加。

---
+++ src/app/goods-detail/goods-detail/goods-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goods-detail',
+  templateUrl: './goods-detail.component.html',
+  styleUrls: ['./goods-detail.component.sass']
+})
+export class GoodsDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
