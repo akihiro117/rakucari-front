ba698b4986b3735b1bba8781cc38b7f483ece7f8
Akihiro Yamada <u.parr917@gmail.com>
2019-08-15 11:01:10 +0900

    商品詳細ページのrouting.tsに商品詳細ページへのパスを追加。

--- src/app/goods-detail/goods-detail-routing.module.ts
+++ src/app/goods-detail/goods-detail-routing.module.ts
@@ -1,7 +1,10 @@
+import { GoodsDetailComponent } from './goods-detail/goods-detail.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GoodsDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
