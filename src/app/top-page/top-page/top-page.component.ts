6d9cc500fb878eeb82e518e6e1948d75417b5283
Akihiro Yamada <u.parr917@gmail.com>
2019-08-15 11:12:51 +0900

    最近追加された商品から商品詳細ページへの画面遷移を実装。

--- src/app/top-page/top-page/top-page.component.ts
+++ src/app/top-page/top-page/top-page.component.ts
@@ -1,6 +1,7 @@
 import { RecentGoodsResponse } from './recent-goods-response';
 import { RecentGoodsService } from './recent-goods.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-page',
@@ -12,7 +13,11 @@ export class TopPageComponent implements OnInit {
   // 最近追加された商品情報。
   public recentAddedGoods: RecentGoodsResponse;
 
-  constructor(private recentGoodsService: RecentGoodsService) { }
+  constructor(
+    private recentGoodsService: RecentGoodsService,
+    // routerを利用した画面遷移で必要。
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.fetchRecentAddedGoods();
@@ -26,6 +31,13 @@ export class TopPageComponent implements OnInit {
       .subscribe(response => {
         this.recentAddedGoods = response;
       });
+  }
+
+  /**
+   * 最近追加された商品をクリックした時に商品詳細ページに遷移する。
+   */
+  public moveToGoodsDetailPage() {
+    this.router.navigate(['/goods-detail']);
   }
 
 }
