9b7276ef5c42ee347084b749e762601ab9511876
Akihiro Yamada <u.parr917@gmail.com>
2019-08-15 10:36:21 +0900

    商品詳細ページのmoduleを作成。

---
+++ src/app/goods-detail/goods-detail.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GoodsDetailRoutingModule } from './goods-detail-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GoodsDetailRoutingModule
+  ]
+})
+export class GoodsDetailModule { }
