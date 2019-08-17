a16867045fe6f9872699668721b52e57dc4e27cd
Akihiro Yamada <u.parr917@gmail.com>
2019-08-17 13:58:18 +0900

    商品情報を取得するメソッドを実装。

---
+++ src/app/goods-detail/goods-detail/goods-detail-response.ts
@@ -0,0 +1,8 @@
+import { Goods } from './goods';
+
+/**
+ * 商品詳細を取得するAPIからのレスポンスを扱うクラス。
+ */
+export class GoodsDetailResponse {
+  public goodsDetail: Goods;
+}
--- src/app/goods-detail/goods-detail/goods-detail.component.ts
+++ src/app/goods-detail/goods-detail/goods-detail.component.ts
@@ -1,3 +1,4 @@
+import { GoodsDetailService } from './goods-detail.service';
 import { Component, OnInit } from '@angular/core';
 import { Goods } from './goods';
 
@@ -11,20 +12,22 @@ export class GoodsDetailComponent implements OnInit {
   // 商品の詳細情報。
   public goods: Goods = new Goods();
 
-  constructor() { }
+  constructor(
+    private goodsDetailService: GoodsDetailService
+  ) { }
 
   ngOnInit() {
-    // ダミーのデータ。
-    // TODO:APIを作成したら、APIから取得する処理をかく。
-    this.goods.goodsName = '商品1';
-    this.goods.imgUrl = '../../assets/images/dummy-goods1/dummy1.png';
-    this.goods.category = 'カテゴリー1';
-    this.goods.goodsStatus = '未使用';
-    this.goods.feeResponsibility = '出品者負担';
-    this.goods.srcRegion = '東京都';
-    this.goods.shippingDate = '2〜3日で発送';
-    this.goods.price = 1200;
-    this.goods.description = 'これはダミーの商品です。';
+    this.fetchGoodsDetail();
+  }
+
+  /**
+   * serviceから商品情報を取得して、商品情報の変数に格納する。
+   */
+  public fetchGoodsDetail(): void {
+    this.goodsDetailService.fetchGoodsDetail('1')
+      .subscribe(response => {
+        this.goods = response.goodsDetail;
+      });
   }
 
 }
