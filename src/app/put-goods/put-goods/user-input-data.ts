e889e79ba59a9ec6599e6af64d3a8d168de89f3d
Akihiro Yamada <u.parr917@gmail.com>
2019-09-23 22:49:44 +0900

    軽微な記述漏れを修正。

--- src/app/put-goods/put-goods/user-input-data.ts
+++ src/app/put-goods/put-goods/user-input-data.ts
@@ -3,8 +3,11 @@ import { SrcRegion } from '../../common/goods-options/src-region';
 import { FeeResponsibility } from '../../common/goods-options/fee-responsibility';
 import { UseStatus } from '../../common/goods-options/use-status';
 import { Category } from 'src/app/common/header/category';
-import { Goods } from 'src/app/top-page/top-page/goods'
+import { Goods } from 'src/app/top-page/top-page/goods';
 
+/**
+ * ユーザーの入力データを扱うクラス。
+ */
 export class UserInputData {
   public goods: Goods = new Goods();
 
